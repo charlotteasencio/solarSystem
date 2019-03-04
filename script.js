9e8ff8f2a0d1c6dfe6ecfa57d416d608c82fbfb3
charlotteasencio <charlotte.e.asencio@gmail.com>
2019-03-03 19:59:06 -0500

    add more 'planets'

--- script.js
+++ script.js
@@ -97,6 +97,8 @@ let planet3;
 let planet4;
 let planet5;
 let planet6;
+let planet7;
+let planet8;
 
 let orbit1;
 let orbit2;
@@ -104,26 +106,41 @@ let orbit3;
 let orbit4;
 let orbit5;
 let orbit6;
+let orbit7;
+let orbit8;
 
 function init() {
     const allX = canvas.width / 2
     const allY = canvas.height / 2
 
+    const planet1dist = 70
+    const planet2dist = 100
+    const planet3dist = 150
+    const planet4dist = 195
+    const planet5dist = 260
+    const planet6dist = 320
+    const planet7dist = 360
+    const planet8dist = 450
+
     sun = new Sun(allX, allY, 35, "yellow")
 
-    planet1 = new Planet(allX, allY, 10, "brown", 70)
-    planet2 = new Planet(allX, allY, 15, "orange", 100)
-    planet3 = new Planet(allX, allY, 15, "blue", 150)
-    planet4 = new Planet(allX, allY, 20, "pink", 195)
-    planet5 = new Planet(allX, allY, 24, "gray", 260)
-    planet6 = new Planet(allX, allY, 30, "green", 360)
+    planet1 = new Planet(allX, allY, 7, "brown", planet1dist)
+    planet2 = new Planet(allX, allY, 10, "orange", planet2dist)
+    planet3 = new Planet(allX, allY, 15, "blue", planet3dist)
+    planet4 = new Planet(allX, allY, 12, "pink", planet4dist)
+    planet5 = new Planet(allX, allY, 24, "tan", planet5dist)
+    planet6 = new Planet(allX, allY, 30, "green", planet6dist)
+    planet7 = new Planet(allX, allY, 18, "lightblue", planet7dist)
+    planet8 = new Planet(allX, allY, 22, "purple", planet8dist)
 
-    orbit1 = new Orbit(allX, allY, 70, "black")
-    orbit2 = new Orbit(allX, allY, 100, "black")
-    orbit3 = new Orbit(allX, allY, 150, "black")
-    orbit4 = new Orbit(allX, allY, 195, "black")
-    orbit5 = new Orbit(allX, allY, 260, "black")
-    orbit6 = new Orbit(allX, allY, 360, "black")
+    orbit1 = new Orbit(allX, allY, planet1dist, "black")
+    orbit2 = new Orbit(allX, allY, planet2dist, "black")
+    orbit3 = new Orbit(allX, allY, planet3dist, "black")
+    orbit4 = new Orbit(allX, allY, planet4dist, "black")
+    orbit5 = new Orbit(allX, allY, planet5dist, "black")
+    orbit6 = new Orbit(allX, allY, planet6dist, "black")
+    orbit7 = new Orbit(allX, allY, planet7dist, "black")
+    orbit8 = new Orbit(allX, allY, planet8dist, "black")
 }
 
 // Animation Loop
@@ -139,6 +156,8 @@ function animate() {
     orbit4.update();
     orbit5.update();
     orbit6.update();
+    orbit7.update();
+    orbit8.update();
 
     planet1.update();
     planet2.update();
@@ -146,6 +165,8 @@ function animate() {
     planet4.update();
     planet5.update();
     planet6.update();
+    planet7.update();
+    planet8.update();
 }
 
 init()
